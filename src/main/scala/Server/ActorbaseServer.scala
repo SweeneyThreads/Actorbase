d25aabaf8247a3d1b899b45eacb87224771b363c
Luca Nicoletti <lucawrath@gmail.com>
2016-04-17 16:54:33 +0200

    Added the beginning of the project

---
+++ src/main/scala/Server/ActorbaseServer.scala
@@ -0,0 +1,20 @@
+package Server
+
+import java.util.logging.{FileHandler, Logger, SimpleFormatter}
+
+/**
+  * Created by lucan on 17/04/2016.
+  */
+
+object ActorbaseServer extends App {
+
+  var myLog : Logger = Logger.getLogger("myLog")
+  var fileHandler : FileHandler = new FileHandler("./MyLogFile.log")
+  myLog.addHandler(fileHandler)
+  var formatter : SimpleFormatter = new SimpleFormatter
+  fileHandler.setFormatter(formatter)
+
+  myLog.info("Server Started correctly")
+
+  myLog.setUseParentHandlers(false);
+}
