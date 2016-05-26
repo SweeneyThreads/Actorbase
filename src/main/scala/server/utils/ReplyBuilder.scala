f73e870a309e89b4625d17f5e28907ba482b9da7
biggeb4 <biggeb4@gmail.com>
2016-05-26 17:36:02 +0200

    implemented ReplyBuilder reply build method

--- src/main/scala/server/utils/ReplyBuilder.scala
+++ src/main/scala/server/utils/ReplyBuilder.scala
@@ -1,13 +1,271 @@
 package server.utils
 
-import server.messages.query.ReplyMessage
+import akka.actor.Props
+import server.Server
+import server.actors.Storemanager
+import server.enums.EnumPermission.UserPermission
+import server.enums.EnumReplyResult
+import server.messages.internal.AskMapMessage
+import server.messages.query.HelpMessages._
+import server.messages.query.PermissionMessages.{NoWritePermissionInfo, NoReadPermissionInfo}
+import server.messages.query.admin.ActorPropetiesMessages.ActorPropertiesMessage
+import server.messages.query.admin.AdminMessage
+import server.messages.query.admin.PermissionsManagementMessages.{RemovePermissionMessage, AddPermissionMessage, ListPermissionMessage, PermissionsManagementMessage}
+import server.messages.query.admin.UsersManagementMessages.{RemoveUserMessage, AddUserMessage, ListUserMessage, UsersManagementMessage}
+import server.messages.query.user.DatabaseMessages._
+import server.messages.query.user.MapMessages._
+import server.messages.query.user.RowMessages._
+import server.messages.query.user.UserMessage
+import server.messages.query.{ReplyInfo, QueryMessage, ReplyMessage}
+
+import scala.util.{Failure, Success}
 
 /**
   * Created by borto on 26/05/2016.
   */
 class ReplyBuilder {
+  /** transform a ReplyMessage into a String */
   def buildReply(reply: ReplyMessage) : String = {
-    ""
+    reply.question match {
+      case m: UserMessage => UserMessageReply(reply)
+      case m: AdminMessage => AdminMessageReply(reply)
+      case _ => ""//TODO
+    }
+  }
+
+  /**create reply String from UserMessage  */
+  private def UserMessageReply(reply: ReplyMessage): String = {
+    reply.question match {
+      case m: HelpMessage => HelpMessageReply(reply)
+      case m: DatabaseMessage => DatabaseMessageReply(reply)
+      case m: MapMessage => MapMessageReply(reply)
+      case m: RowMessage => RowMessageReply(reply)
+      case _ =>"" //TODO
+    }
+  }
+
+  /** create reply String from admin query messages (UserManagement, PermissionManagement and Properties) */
+  private def AdminMessageReply(reply: ReplyMessage): String = {
+    reply.question match {
+      case m:UsersManagementMessage => UserManagementMessageReply(reply)
+      case m:PermissionsManagementMessage => PermissionsManagementMessageReply(reply)
+      case m:ActorPropertiesMessage =>  ActorPropertiesMessageMessageReply(reply)
+      case _ => ""//TODO
+    }
+  }
+
+  /** create reply String from user management messages (ListUser, AddUser and RemoveUser) */
+  private def UserManagementMessageReply(reply: ReplyMessage): String = {
+    reply.question match {
+      case ListUserMessage() =>"" //TODO
+      case AddUserMessage(username: String, password:String) => ""//TODO
+      case RemoveUserMessage(username: String) => ""//TODO
+      case _ =>"" //TODO
+    }
+  }
+
+  /** create reply String from permission management messages (ListPermission, AddPermission, RemovePermission) */
+  private def PermissionsManagementMessageReply(reply: ReplyMessage): String = {
+    reply.question match {
+      case ListPermissionMessage(username:String) =>"" //TODO
+      case AddPermissionMessage(username: String, database:String, permissionType: UserPermission) =>"" //TODO
+      case RemovePermissionMessage(username: String, database:String) =>"" //TODO
+      case _ =>"" //TODO
+    }
+  }
+
+  /** create reply String from actors' properties messages */
+  private def ActorPropertiesMessageMessageReply(reply: ReplyMessage): String = {
+    reply.question match {
+      case _ =>"" //TODO
+    }
+  }
+
+  /** create String from help messages (complete or specific ones) */
+  private def HelpMessageReply(reply: ReplyMessage): String = {
+    reply.result match {
+      case EnumReplyResult.Done => DoneHelpMessageReply(reply.question,reply.info)
+      case EnumReplyResult.Error => "" //TODO
+      case _ => "" //TODO
+    }
+  }
+  /**create final string for Done Help*/
+  private def DoneHelpMessageReply(question:QueryMessage,info:ReplyInfo): String ={
+    question match{
+      case CompleteHelp() => {
+        info match {
+          case CompleteHelpReplyInfo(commands: String) => commands
+          case _ => "" //TODO
+        }
+      }
+      case SpecificHelp =>{
+        info match {
+          case SpecificHelpReplyInfo(command: String) => command
+          case _ => "" //TODO
+        }
+      }
+    }
+  }
+  /** create reply String for database messages (ListDatabase, SelectDatabase, CreateDatabase and DeleteDatabase) */
+  private def DatabaseMessageReply(reply: ReplyMessage): String = {
+    reply.result match {
+      case EnumReplyResult.Done =>{
+        reply.question match{
+          case ListDatabaseMessage() => {
+            reply.info match {
+              case ListDBInfo(dbs: List[String]) => dbs.mkString(" ")
+              case _ => "" //TODO
+            }
+          }
+          case SelectDatabaseMessage(name: String) => "database "+name+" selected"
+          case CreateDatabaseMessage(name: String) => "database "+name+" created"
+          case DeleteDatabaseMessage(name: String) => "database "+name+" deleted"
+          case _ =>"" //TODO
+        }
+      }
+      case EnumReplyResult.Error =>{
+        reply.question match{
+          case ListDatabaseMessage() => {
+            reply.info match {
+              case NoDBInfo() => "no database found"
+              case _ => ""//TODO
+            }
+          }
+          case SelectDatabaseMessage(name: String) =>{
+            reply.info match {
+              case DBDoesNotExistInfo() => "database "+name+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case CreateDatabaseMessage(name: String) => {
+            reply.info match {
+              case DBAlreadyExistInfo() => "the database name: "+name+" is already used"
+              case _ => ""//TODO
+            }
+          }
+          case DeleteDatabaseMessage(name: String) => {
+            reply.info match {
+              case DBDoesNotExistInfo() => "database "+name+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case _ => ""//TODO
+        }
+      }
+      case _ => ""//TODO
+    }
+  }
+
+  /** create reply string for map messages */
+  private def MapMessageReply(reply: ReplyMessage): String = {
+    reply.result match {
+      case EnumReplyResult.Done =>{
+        reply.question match{
+          case ListMapMessage() => {
+            reply.info match {
+              case ListMapInfo(dbs: List[String]) => dbs.mkString(" ")
+              case _ => "" //TODO
+            }
+          }
+          case SelectMapMessage(name: String) => "map "+name+" selected"
+          case CreateMapMessage(name: String) => "map "+name+" created"
+          case DeleteMapMessage(name: String) => "map "+name+" deleted"
+          case _ =>"" //TODO
+        }
+      }
+      case EnumReplyResult.Error =>{
+        reply.question match{
+          case ListMapMessage() => {
+            reply.info match {
+              case NoMapInfo() => "no map found"
+              case _ => ""//TODO
+            }
+          }
+          case SelectMapMessage(name: String) =>{
+            reply.info match {
+              case MapDoesNotExistInfo() => "map "+name+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case CreateMapMessage(name: String) => {
+            reply.info match {
+              case MapAlreadyExistInfo() => "the map name: "+name+" is already used"
+              case _ => ""//TODO
+            }
+          }
+          case DeleteMapMessage(name: String) => {
+            reply.info match {
+              case MapDoesNotExistInfo() => "map "+name+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case _ => ""//TODO
+        }
+      }
+      case _ => ""//TODO
+    }
+  }
+
+  /** Manages row messages */
+  private def RowMessageReply(reply: ReplyMessage): String = {
+    reply.result match {
+      case EnumReplyResult.Done =>{
+        reply.question match{
+          case ListKeysMessage() => {
+            reply.info match {
+              case ListKeyInfo(dbs: List[String]) => dbs.mkString(" ")
+              case _ => "" //TODO
+            }
+          }
+          case FindRowMessage() =>{
+            reply.info match {
+              case FindInfo(value: String) => value
+              case _ => "" //TODO
+            }
+          }
+          case InsertRowMessage(key: String, value: String) => "key: "+key+" with value: "+value+" inserted"
+          case UpdateRowMessage(key: String, value: String) => "key: "+key+" updated with value: "+value
+          case RemoveRowMessage(key: String) => "key: "+key+" deleted"
+          case _ =>"" //TODO
+        }
+      }
+      case EnumReplyResult.Error =>{
+        reply.question match{
+          case ListKeysMessage() => {
+            reply.info match {
+              case NoKeyInfo() => "no key found"
+              case _ => ""//TODO
+            }
+          }
+          case FindRowMessage(key: String) =>{
+            reply.info match {
+              case KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case InsertRowMessage(key: String) => {
+            reply.info match {
+              case KeyAlreadyExistInfo() => "key: "+key+" is already used"
+              case _ => ""//TODO
+            }
+          }
+          case UpdateRowMessage(key: String) => {
+            reply.info match {
+              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case RemoveRowMessage(key: String) => {
+            reply.info match {
+              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case _ => ""//TODO
+            }
+          }
+          case _ => ""//TODO
+        }
+      }
+      case _ => ""//TODO
+    }
   }
 
   def unhandledMessage(actor: String, method: String) : String = {
