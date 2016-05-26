711613871db46d57e0279bf19275efe0e82a8527
biggeb4 <biggeb4@gmail.com>
2016-05-26 22:42:15 +0200

    ripristined from sync error and added comments

--- src/main/scala/server/utils/ReplyBuilder.scala
+++ src/main/scala/server/utils/ReplyBuilder.scala
@@ -1,17 +1,24 @@
 package server.utils
 
+import akka.actor.Props
+import server.Server
+import server.actors.Storemanager
 import server.enums.EnumPermission.UserPermission
 import server.enums.EnumReplyResult
+import server.messages.internal.AskMapMessage
 import server.messages.query.HelpMessages._
+import server.messages.query.PermissionMessages.{NoWritePermissionInfo, NoReadPermissionInfo}
 import server.messages.query.admin.ActorPropetiesMessages.ActorPropertiesMessage
 import server.messages.query.admin.AdminMessage
-import server.messages.query.admin.PermissionsManagementMessages.{AddPermissionMessage, ListPermissionMessage, PermissionsManagementMessage, RemovePermissionMessage}
-import server.messages.query.admin.UsersManagementMessages.{AddUserMessage, ListUserMessage, RemoveUserMessage, UsersManagementMessage}
+import server.messages.query.admin.PermissionsManagementMessages.{RemovePermissionMessage, AddPermissionMessage, ListPermissionMessage, PermissionsManagementMessage}
+import server.messages.query.admin.UsersManagementMessages.{RemoveUserMessage, AddUserMessage, ListUserMessage, UsersManagementMessage}
 import server.messages.query.user.DatabaseMessages._
 import server.messages.query.user.MapMessages._
 import server.messages.query.user.RowMessages._
 import server.messages.query.user.UserMessage
-import server.messages.query.{QueryMessage, ReplyInfo, ReplyMessage}
+import server.messages.query.{ReplyInfo, QueryMessage, ReplyMessage}
+
+import scala.util.{Failure, Success}
 
 /**
   * Created by borto on 26/05/2016.
@@ -88,13 +95,13 @@ class ReplyBuilder {
       case CompleteHelp() => {
         info match {
           case CompleteHelpReplyInfo(commands: String) => commands
-          case _ => "" //TODO
+          case _ => "Unknown done CompleteHelp info" //TODO
         }
       }
       case SpecificHelp(command: String) =>{
         info match {
           case SpecificHelpReplyInfo(command: String) => command
-          case _ => "" //TODO
+          case _ => "Unknown done SpecificHelp info" //TODO
         }
       }
     }
@@ -102,162 +109,212 @@ class ReplyBuilder {
   /** create reply String for database messages (ListDatabase, SelectDatabase, CreateDatabase and DeleteDatabase) */
   private def DatabaseMessageReply(reply: ReplyMessage): String = {
     reply.result match {
+        // db message question done
       case EnumReplyResult.Done =>{
         reply.question match{
           case ListDatabaseMessage() => {
+            //info for a done ListDatabaseMessage
             reply.info match {
-              case ListDBInfo(dbs: List[String]) => dbs.mkString(" ")
-              case _ => "" //TODO
+              case ListDBInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
+              case _ => "Unknown info on done ListDatabaseMessage" //TODO
             }
           }
-          case SelectDatabaseMessage(name: String) => "database "+name+" selected"
-          case CreateDatabaseMessage(name: String) => "database "+name+" created"
-          case DeleteDatabaseMessage(name: String) => "database "+name+" deleted"
-          case _ =>"" //TODO
+          case SelectDatabaseMessage(name: String) => "Database "+name+" selected"
+          case CreateDatabaseMessage(name: String) => "Database "+name+" created"
+          case DeleteDatabaseMessage(name: String) => "Database "+name+" deleted"
+          case _ =>"Unknown question on done db message reply" //TODO
         }
       }
+        // db message question not done or anomaly done
       case EnumReplyResult.Error =>{
         reply.question match{
           case ListDatabaseMessage() => {
             reply.info match {
-              case NoDBInfo() => "no database found"
-              case _ => ""//TODO
+              //info for an error ListDatabaseMessage
+              case NoDBInfo() => "No database found"
+              case _ => "Unknown info on error ListDatabaseMessage"//TODO
             }
           }
           case SelectDatabaseMessage(name: String) =>{
             reply.info match {
-              case DBDoesNotExistInfo() => "database "+name+" not exist"
-              case _ => ""//TODO
+              //info for an error SelectDatabaseMessage
+              case DBDoesNotExistInfo() => "Database "+name+" not exist"
+              case NoReadPermissionInfo() => "No read permission"
+              case _ => "Unknown info on error SelectDatabaseMessage"//TODO
             }
           }
           case CreateDatabaseMessage(name: String) => {
             reply.info match {
-              case DBAlreadyExistInfo() => "the database name: "+name+" is already used"
-              case _ => ""//TODO
+              //info for an error CreateDatabaseMessage
+              case DBAlreadyExistInfo() => "The database name "+name+" is already used"
+              case _ => "Unknown info on error CreateDatabaseMessage"//TODO
             }
           }
           case DeleteDatabaseMessage(name: String) => {
             reply.info match {
-              case DBDoesNotExistInfo() => "database "+name+" not exist"
-              case _ => ""//TODO
+                //info for an error DeleteDatabaseMessage
+              case NoWritePermissionInfo() => "No write permission "
+              case DBDoesNotExistInfo() => "Database "+name+" not exist"
+              case _ => "Unknown info on error DeleteDatabaseMessage"//TODO
             }
           }
-          case _ => ""//TODO
+          case _ => "Unknown question on error db message reply"//TODO
         }
       }
-      case _ => ""//TODO
+      case _ => "Unknown result on db message reply "//TODO
     }
   }
 
   /** create reply string for map messages */
   private def MapMessageReply(reply: ReplyMessage): String = {
     reply.result match {
+      //question was done
       case EnumReplyResult.Done =>{
         reply.question match{
           case ListMapMessage() => {
             reply.info match {
-              case ListMapInfo(dbs: List[String]) => dbs.mkString(" ")
-              case _ => "" //TODO
+                //info for a done ListMapMessage
+              case ListMapInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
+              case _ => "Unknown info for a done ListMapMessage" //TODO
             }
           }
-          case SelectMapMessage(name: String) => "map "+name+" selected"
-          case CreateMapMessage(name: String) => "map "+name+" created"
-          case DeleteMapMessage(name: String) => "map "+name+" deleted"
-          case _ =>"" //TODO
+          case SelectMapMessage(name: String) => "Map "+name+" selected"
+          case CreateMapMessage(name: String) => "Map "+name+" created"
+          case DeleteMapMessage(name: String) => "Map "+name+" deleted"
+          case _ =>"Question done map message unknown" //TODO
         }
       }
+      //the question wasn't normally done
       case EnumReplyResult.Error =>{
+        //check if db wasn't selected error
+        if(reply.info.isInstanceOf[NoDBSelectedInfo]){
+          return "Select database first"
+        }
+        //check if there was a read permission error
+        if(reply.info.isInstanceOf[NoReadPermissionInfo]){
+          return "No read permission"
+        }
         reply.question match{
           case ListMapMessage() => {
             reply.info match {
-              case NoMapInfo() => "no map found"
-              case _ => ""//TODO
+              //info for a error ListMapMessage
+              case NoMapInfo() => "No map found"
+              case _ => "Unknown info for a error ListMapMessage"//TODO
             }
           }
           case SelectMapMessage(name: String) =>{
             reply.info match {
-              case MapDoesNotExistInfo() => "map "+name+" not exist"
-              case _ => ""//TODO
+              //info for a error SelectMapMessage
+              case MapDoesNotExistInfo() => "Map "+name+" not exist"
+              case _ => "Unknown info for a error ListMapMessage"//TODO
             }
           }
           case CreateMapMessage(name: String) => {
             reply.info match {
-              case MapAlreadyExistInfo() => "the map name: "+name+" is already used"
-              case _ => ""//TODO
+              //info for a error CreateMapMessage
+              case MapAlreadyExistInfo() => "The map name "+name+" is already used"
+              case NoWritePermissionInfo() => "No write permission"
+              case _ => "Unknown info for a error CreateMapMessage"//TODO
             }
           }
           case DeleteMapMessage(name: String) => {
             reply.info match {
-              case MapDoesNotExistInfo() => "map "+name+" not exist"
-              case _ => ""//TODO
+              //info for a error DeleteMapMessage
+              case MapDoesNotExistInfo() => "Map "+name+" not exist"
+              case NoWritePermissionInfo() => "No write permission"
+              case _ => "Unknown info for a error DeleteMapMessage"//TODO
             }
           }
-          case _ => ""//TODO
+          case _ => "Unknown question on error map message reply"//TODO
         }
       }
-      case _ => ""//TODO
+      case _ => "Unknown result on map message reply"//TODO
     }
   }
 
-  /** Manages row messages */
+  /** create reply string for row messages */
   private def RowMessageReply(reply: ReplyMessage): String = {
     reply.result match {
+        //the question was correctly done
       case EnumReplyResult.Done =>{
         reply.question match{
           case ListKeysMessage() => {
+            //possible info for a done ListKeysMessage
             reply.info match {
-              case ListKeyInfo(dbs: List[String]) => dbs.mkString(" ")
-              case _ => "" //TODO
+              case ListKeyInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
+              case _ => "Unknown info for ListKeysMessage done" //TODO
             }
           }
           case FindRowMessage(key: String) =>{
             reply.info match {
+              //possible info for a done FindRowMessage
               case FindInfo(value: String) => value
-              case _ => "" //TODO
+              case _ => "Unknown info for FindRowMessage done" //TODO
             }
           }
-          case InsertRowMessage(key: String, value: String) => "key: "+key+" with value: "+value+" inserted"
-          case UpdateRowMessage(key: String, value: String) => "key: "+key+" updated with value: "+value
-          case RemoveRowMessage(key: String) => "key: "+key+" deleted"
-          case _ =>"" //TODO
+          case InsertRowMessage(key: String, value: String) => "Key "+key+" with value "+value+" inserted"
+          case UpdateRowMessage(key: String, value: String) => "Key "+key+" updated with value "+value
+          case RemoveRowMessage(key: String) => "Key "+key+" deleted"
+          case _ =>"Unknown done row message question " //TODO
         }
       }
+        //the question wasn't normally done
       case EnumReplyResult.Error =>{
+        //check if db wasn't selected error
+        if(reply.info.isInstanceOf[NoDBSelectedInfo]){
+          return "Select database and map first"
+        }
+        //check if map wasn't selected error
+        if(reply.info.isInstanceOf[NoMapSelectedInfo]){
+          return "Select map first"
+        }
+        //check if there was a read permission error
+        if(reply.info.isInstanceOf[NoReadPermissionInfo]){
+          return "No read permission"
+        }
         reply.question match{
           case ListKeysMessage() => {
+            //possible info for a failed ListKeysMessage
             reply.info match {
-              case NoKeyInfo() => "no key found"
-              case _ => ""//TODO
+              case NoKeyInfo() => "No key found"
+              case _ => "Unknown info for ListKeysMessage error"//TODO
             }
           }
           case FindRowMessage(key: String) =>{
+            //possible info for a failed FindRowMessage
             reply.info match {
-              case KeyDoesNotExistInfo() => "key: "+key+" not exist"
-              case _ => ""//TODO
+              case KeyDoesNotExistInfo() => "Key "+key+" not exist"
+              case _ => "Unknown info for FindRowMessage error"//TODO
             }
           }
           case InsertRowMessage(key: String, value: String) => {
             reply.info match {
-              case KeyAlreadyExistInfo() => "key: "+key+" is already used"
-              case _ => ""//TODO
+              //possible info for a failed InsertRowMessage
+              case KeyAlreadyExistInfo() => "Key "+key+" is already used"
+              case NoWritePermissionInfo() => "No write permission"
+              case _ => "Unknown info for InsertRowMessage  error"//TODO
             }
           }
-          case UpdateRowMessage(key: String, value:String) => {
+          case UpdateRowMessage(key: String, value: String) => {
             reply.info match {
-              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
-              case _ => ""//TODO
+              //possible info for a failed UpdateRowMessage
+              case  KeyDoesNotExistInfo() => "Key "+key+" not exist"
+              case NoWritePermissionInfo() => "No write permission"
+              case _ => "Unknown info for UpdateRowMessage error"//TODO
             }
           }
           case RemoveRowMessage(key: String) => {
             reply.info match {
-              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
-              case _ => ""//TODO
+              //possible info for a failed RemoveRowMessage
+              case  KeyDoesNotExistInfo() => "Key "+key+" not exist"
+              case NoWritePermissionInfo() => "No write permission"
+              case _ => "Unknown info for RemoveRowMessage error"//TODO
             }
           }
-          case _ => ""//TODO
+          case _ => "Unknown question on error row message reply"//TODO
         }
       }
-      case _ => ""//TODO
+      case _ => "Unknown result on row message reply"//TODO
     }
   }
 
