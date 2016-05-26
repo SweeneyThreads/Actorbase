d5067cd52aeefb2f4650456ea664720903da08a9
biggeb4 <biggeb4@gmail.com>
2016-05-26 18:52:01 +0200

    replyBuilder corrected

--- src/main/scala/server/utils/ReplyBuilder.scala
+++ src/main/scala/server/utils/ReplyBuilder.scala
@@ -98,7 +98,7 @@ class ReplyBuilder {
           case _ => "" //TODO
         }
       }
-      case SpecificHelp =>{
+      case SpecificHelp(command: String) =>{
         info match {
           case SpecificHelpReplyInfo(command: String) => command
           case _ => "" //TODO
@@ -113,13 +113,13 @@ class ReplyBuilder {
         reply.question match{
           case ListDatabaseMessage() => {
             reply.info match {
-              case ListDBInfo(dbs: List[String]) => dbs.mkString(" ")
+              case ListDBInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
               case _ => "" //TODO
             }
           }
-          case SelectDatabaseMessage(name: String) => "database "+name+" selected"
-          case CreateDatabaseMessage(name: String) => "database "+name+" created"
-          case DeleteDatabaseMessage(name: String) => "database "+name+" deleted"
+          case SelectDatabaseMessage(name: String) => "Database "+name+" selected"
+          case CreateDatabaseMessage(name: String) => "Database "+name+" created"
+          case DeleteDatabaseMessage(name: String) => "Database "+name+" deleted"
           case _ =>"" //TODO
         }
       }
@@ -127,25 +127,25 @@ class ReplyBuilder {
         reply.question match{
           case ListDatabaseMessage() => {
             reply.info match {
-              case NoDBInfo() => "no database found"
+              case NoDBInfo() => "No database found"
               case _ => ""//TODO
             }
           }
           case SelectDatabaseMessage(name: String) =>{
             reply.info match {
-              case DBDoesNotExistInfo() => "database "+name+" not exist"
+              case DBDoesNotExistInfo() => "Database "+name+" not exist"
               case _ => ""//TODO
             }
           }
           case CreateDatabaseMessage(name: String) => {
             reply.info match {
-              case DBAlreadyExistInfo() => "the database name: "+name+" is already used"
+              case DBAlreadyExistInfo() => "The database name: "+name+" is already used"
               case _ => ""//TODO
             }
           }
           case DeleteDatabaseMessage(name: String) => {
             reply.info match {
-              case DBDoesNotExistInfo() => "database "+name+" not exist"
+              case DBDoesNotExistInfo() => "Database "+name+" not exist"
               case _ => ""//TODO
             }
           }
@@ -163,39 +163,42 @@ class ReplyBuilder {
         reply.question match{
           case ListMapMessage() => {
             reply.info match {
-              case ListMapInfo(dbs: List[String]) => dbs.mkString(" ")
+              case ListMapInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
               case _ => "" //TODO
             }
           }
-          case SelectMapMessage(name: String) => "map "+name+" selected"
-          case CreateMapMessage(name: String) => "map "+name+" created"
-          case DeleteMapMessage(name: String) => "map "+name+" deleted"
+          case SelectMapMessage(name: String) => "Map "+name+" selected"
+          case CreateMapMessage(name: String) => "Map "+name+" created"
+          case DeleteMapMessage(name: String) => "Map "+name+" deleted"
           case _ =>"" //TODO
         }
       }
       case EnumReplyResult.Error =>{
+        if(reply.info.isInstanceOf[NoDBSelectedInfo]){
+          return "Select database first"
+        }
         reply.question match{
           case ListMapMessage() => {
             reply.info match {
-              case NoMapInfo() => "no map found"
+              case NoMapInfo() => "No map found"
               case _ => ""//TODO
             }
           }
           case SelectMapMessage(name: String) =>{
             reply.info match {
-              case MapDoesNotExistInfo() => "map "+name+" not exist"
+              case MapDoesNotExistInfo() => "Map "+name+" not exist"
               case _ => ""//TODO
             }
           }
           case CreateMapMessage(name: String) => {
             reply.info match {
-              case MapAlreadyExistInfo() => "the map name: "+name+" is already used"
+              case MapAlreadyExistInfo() => "The map name: "+name+" is already used"
               case _ => ""//TODO
             }
           }
           case DeleteMapMessage(name: String) => {
             reply.info match {
-              case MapDoesNotExistInfo() => "map "+name+" not exist"
+              case MapDoesNotExistInfo() => "Map "+name+" not exist"
               case _ => ""//TODO
             }
           }
@@ -213,51 +216,57 @@ class ReplyBuilder {
         reply.question match{
           case ListKeysMessage() => {
             reply.info match {
-              case ListKeyInfo(dbs: List[String]) => dbs.mkString(" ")
+              case ListKeyInfo(dbs: List[String]) => dbs.mkString("/n").dropRight(1)
               case _ => "" //TODO
             }
           }
-          case FindRowMessage() =>{
+          case FindRowMessage(key: String) =>{
             reply.info match {
               case FindInfo(value: String) => value
               case _ => "" //TODO
             }
           }
-          case InsertRowMessage(key: String, value: String) => "key: "+key+" with value: "+value+" inserted"
-          case UpdateRowMessage(key: String, value: String) => "key: "+key+" updated with value: "+value
-          case RemoveRowMessage(key: String) => "key: "+key+" deleted"
+          case InsertRowMessage(key: String, value: String) => "Key: "+key+" with value: "+value+" inserted"
+          case UpdateRowMessage(key: String, value: String) => "Key: "+key+" updated with value: "+value
+          case RemoveRowMessage(key: String) => "Key: "+key+" deleted"
           case _ =>"" //TODO
         }
       }
       case EnumReplyResult.Error =>{
+        if(reply.info.isInstanceOf[NoDBSelectedInfo]){
+          return "Select database and map first"
+        }
+        if(reply.info.isInstanceOf[NoMapSelectedInfo]){
+          return "Select map first"
+        }
         reply.question match{
           case ListKeysMessage() => {
             reply.info match {
-              case NoKeyInfo() => "no key found"
+              case NoKeyInfo() => "No key found"
               case _ => ""//TODO
             }
           }
           case FindRowMessage(key: String) =>{
             reply.info match {
-              case KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case KeyDoesNotExistInfo() => "Key: "+key+" not exist"
               case _ => ""//TODO
             }
           }
-          case InsertRowMessage(key: String) => {
+          case InsertRowMessage(key: String, value: String) => {
             reply.info match {
-              case KeyAlreadyExistInfo() => "key: "+key+" is already used"
+              case KeyAlreadyExistInfo() => "Key: "+key+" is already used"
               case _ => ""//TODO
             }
           }
-          case UpdateRowMessage(key: String) => {
+          case UpdateRowMessage(key: String, value: String) => {
             reply.info match {
-              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case  KeyDoesNotExistInfo() => "Key: "+key+" not exist"
               case _ => ""//TODO
             }
           }
           case RemoveRowMessage(key: String) => {
             reply.info match {
-              case  KeyDoesNotExistInfo() => "key: "+key+" not exist"
+              case  KeyDoesNotExistInfo() => "Key: "+key+" not exist"
               case _ => ""//TODO
             }
           }
