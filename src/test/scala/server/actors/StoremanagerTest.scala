5cf71fd7947b62452a470203ee8f60b8956d390f
biggeb4 <biggeb4@gmail.com>
2016-05-28 16:08:38 +0200

    storemanager tests complited

--- src/test/scala/server/actors/StoremanagerTest.scala
+++ src/test/scala/server/actors/StoremanagerTest.scala
@@ -6,15 +6,20 @@ import server.messages.query.ReplyMessage
 import scala.language.postfixOps
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 import java.util.concurrent.ConcurrentHashMap
+
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
 import server.messages.query.user.DatabaseMessages._
 import server.messages.query.user.MapMessages._
-import server.utils.{ServerDependencyInjector}
-
+import server.utils.ServerDependencyInjector
 import akka.testkit.TestActorRef
+import server.enums.EnumReplyResult.Done
+import server.messages.internal.AskMapMessage
+import server.messages.query.user.RowMessages._
+
+import scala.util.matching.Regex
 
 /**
   * Created by mattia on 27/05/2016.
@@ -34,14 +39,27 @@ class StoremanagerTest extends FlatSpec with Matchers with MockFactory {
   implicit val system = ActorSystem()
 
   /*########################################################################
-  Testing CreateMapMessage() receiving
-  ########################################################################*/
-  "Storemanager" should "reply correctly to a CreateMapMessage()" in {
-    //creating a StoremanagerActor into system
-    val storemanager = System.actorOf(Props(new Storemanager()))
-    val future = storemanager ? new CreateMapMessage("prova")
+    Testing AskMapMessage() receiving
+    ########################################################################*/
+  /*testing if the storemanager returns the correct reply to yhe Main when reciving an AskMapMessage*/
+
+
+  it should "actually return true if the storemanager contains the map asked with an AskMapMessage" in {
+    // TestActorRef is a exoteric function provided by akka-testkit
+    // it creates a special actorRef that could be used for test purpose
+    val actorRef=TestActorRef(new Storemanager)
+    // retrieving the underlying actor
+    val actor = actorRef.underlyingActor
+    // now I send the message
+    val future = actorRef ? AskMapMessage("defaultMap")
+    //when the message is completed i check that the StoremanagerActor reply correctly
     ScalaFutures.whenReady(future) {
-      result => result should be(new ReplyMessage (EnumReplyResult.Done,new CreateMapMessage("prova"),null))
+      result => result.asInstanceOf[Boolean] should be(true)
+    }
+    val future2 = actorRef ? AskMapMessage("NotexistingMap")
+    //when the message is completed i check that the StoremanagerActor reply correctly
+    ScalaFutures.whenReady(future2) {
+      result => result.asInstanceOf[Boolean] should be(false)
     }
   }
 
@@ -71,6 +89,31 @@ class StoremanagerTest extends FlatSpec with Matchers with MockFactory {
   }
 
   /*########################################################################
+  Testing CreateMapMessage() receiving
+  ########################################################################*/
+  it should "create the correct map and reply correctly or send the correct error when receving a CreateMapMessage" in {
+    // TestActorRef is a exoteric function provided by akka-testkit
+    // it creates a special actorRef that could be used for test purpose
+    val actorRef=TestActorRef(new Storemanager)
+    // retrieving the underlying actor
+    val actor = actorRef.underlyingActor
+    // now I send the message
+    val future = actorRef ? CreateMapMessage("map1")
+    //when the message is completed i check that the StoremanagerActor reply correctly and delete correctly
+    ScalaFutures.whenReady(future) {
+      actor.storefinders.containsKey("map1") should be (true)
+      result => result should be(new ReplyMessage (EnumReplyResult.Done,new CreateMapMessage("map1"),null))
+    }
+    val future2 = actorRef ? CreateMapMessage("map1")
+    //when the message is completed i check that the StoremanagerActor reply correctly that the map already exist
+    ScalaFutures.whenReady(future2) {
+      result => result should be(new ReplyMessage (EnumReplyResult.Error,new CreateMapMessage("map1"),MapAlreadyExistInfo()))
+    }
+  }
+
+
+
+  /*########################################################################
    Testing DeleteMapMessage() receiving
    ########################################################################*/
   /*testing if the storemanager delete the correct map and reply correctly or send the correct error when receving a DeleteMapMessage*/
@@ -92,6 +135,51 @@ class StoremanagerTest extends FlatSpec with Matchers with MockFactory {
       actor.storefinders.contains("map1") should be (false)
       result => result should be(new ReplyMessage (EnumReplyResult.Done,new DeleteMapMessage("map1"),null))
     }
+    val future2 = actorRef ? DeleteMapMessage("NotExistingMap")
+    //when the message is completed i check that the StoremanagerActor reply correctly that the map not exist
+    ScalaFutures.whenReady(future2) {
+      result => result should be(new ReplyMessage (EnumReplyResult.Error,new DeleteMapMessage("NotExistingMap"),MapDoesNotExistInfo()))
+    }
   }
+
+  /*########################################################################
+    Testing RowMapMessage() receiving
+    ########################################################################*/
+  /*testing if the storemanager receive and send correctly RowMapMessages and reply correctly*/
+
+
+
+  it should "actually return the FakeStorefinder answer" in {
+    // TestActorRef is a exoteric function provided by akka-testkit
+    // it creates a special actorRef that could be used for test purpose
+    val actorRef=TestActorRef(new Storemanager)
+    // retrieving the underlying actor
+    val actor = actorRef.underlyingActor
+    // putting extra FakeStorefinder map in the storefinders map
+    actor.storefinders.put("map1",System.actorOf(Props[FakeStorefinder]))
+    // now I send the message
+    val future = actorRef ? StorefinderRowMessage("map1", FindRowMessage("1"))
+    //when the message is completed i check that the StoremanagerActor reply correctly
+    ScalaFutures.whenReady(future) {
+      result => result should be(ReplyMessage(Done, new FindRowMessage("1"), FindInfo("2")))
+    }
+    val future2 = actorRef ? StorefinderRowMessage("map2", FindRowMessage("1"))
+    //when the message is completed i check that the StoremanagerActor reply correctly
+    ScalaFutures.whenReady(future2) {
+      result => result should be(ReplyMessage(EnumReplyResult.Error,StorefinderRowMessage("map2" ,new FindRowMessage("1")), MapDoesNotExistInfo()))
+    }
+  }
+}
+
+/*FakeStoremanage for test porpuse always answers in the same way*/
+class FakeStorefinder extends Storefinder{
+
+  override def receive = {
+    case m:RowMessage => {
+      val origSender = sender
+      reply(ReplyMessage(Done, new FindRowMessage("1"), FindInfo("2")), origSender)
+    }
+  }
+
 }
 
